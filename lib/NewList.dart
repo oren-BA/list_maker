3464f547ad5941dbc48095e6579cdc25f20782b8
oren-BA <orenbenaltabe@gmail.com>
2021-03-07 20:49:57 +0200

    lists are now saved when exiting the page

--- lib/NewList.dart
+++ lib/NewList.dart
@@ -3,22 +3,24 @@ import 'package:flutter/material.dart';
 
 class NewList extends StatefulWidget {
   String name;
-  List<String> items = [] ;
+  List<String> items = [];
+  List<String> checkedItems = [];
+
   NewList({Key key, this.name}) : super(key: key);
+
   @override
   HomePage createState() => HomePage();
-  // HomePage createState() => home;
+// HomePage createState() => home;
 }
 
 class HomePage extends State<NewList> with AutomaticKeepAliveClientMixin {
-
   String inputItem = "";
   String inputCompany = "";
-
+  String item;
+  bool tapped;
 
   void addItem(String inputItem, String inputCompany) {
     setState(() {
-
       if (inputItem == "") {
         return;
       }
@@ -32,6 +34,22 @@ class HomePage extends State<NewList> with AutomaticKeepAliveClientMixin {
     });
   }
 
+  void tappedCheck(int index) {
+    setState(() {
+      item = widget.items[index];
+      widget.items.removeAt(index);
+      widget.checkedItems.add(item);
+    });
+  }
+
+  void tappedUncheck(int index) {
+    setState(() {
+      item = widget.checkedItems[index];
+      widget.checkedItems.removeAt(index);
+      widget.items.add(item);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     super.build(context);
@@ -39,31 +57,105 @@ class HomePage extends State<NewList> with AutomaticKeepAliveClientMixin {
         appBar: AppBar(
           title: Text(widget.name),
         ),
-        body: ListView.builder(
-          itemCount: widget.items.length,
-          itemBuilder: (BuildContext context, int index) {
-            return Dismissible(
-                key: Key(widget.items[index]),
-                child: Card(
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(16)),
-                    elevation: 4,
-                    margin: EdgeInsets.all(4),
-                    child: ListTile(
-                        title: Text(widget.items[index]),
-                        trailing: IconButton(
-                          icon: Icon(
-                            Icons.check,
-                            color: Colors.green,
-                          ),
-                          onPressed: () {
-                            setState(() {
-                              widget.items.removeAt(index);
-                            });
-                          },
-                        ))));
-          },
-        ),
+        body: Column(children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: widget.items.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Dismissible(
+                    key: Key(widget.items[index]),
+                    child: Card(
+                        // color: isTapped ? Colors.grey[300] : Colors.white,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(16)),
+                        elevation: 4,
+                        margin: EdgeInsets.all(4),
+                        child: Container(
+                          // color: ,
+                          child: ListTile(
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(16)),
+                              title: Text(widget.items[index]),
+                              trailing: Row(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    IconButton(
+                                      icon: Icon(
+                                        Icons.delete,
+                                        color: Colors.red[300],
+                                      ),
+                                      onPressed: () {
+                                        setState(() {
+                                          widget.items.removeAt(index);
+                                        });
+                                      },
+                                    ),
+                                    IconButton(
+                                      icon: Icon(
+                                        Icons.check,
+                                        color: Colors.green,
+                                      ),
+                                      onPressed: () {
+                                        tappedCheck(index);
+                                      },
+                                    ),
+                                  ])),
+                        )));
+              },
+            ),
+          ),
+          Divider(
+            thickness: 5,
+            indent: 15,
+            endIndent: 15,
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: widget.checkedItems.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Dismissible(
+                    key: Key(widget.checkedItems[index]),
+                    child: Card(
+                        color: Colors.grey[300],
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(16)),
+                        elevation: 4,
+                        margin: EdgeInsets.all(4),
+                        child: Container(
+                          // color: ,
+                          child: ListTile(
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(16)),
+                              title: Text(widget.checkedItems[index]),
+                              trailing: Row(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    IconButton(
+                                      icon: Icon(
+                                        Icons.delete,
+                                        color: Colors.red[300],
+                                      ),
+                                      onPressed: () {
+                                        setState(() {
+                                          widget.checkedItems.removeAt(index);
+                                        });
+                                      },
+                                    ),
+                                    IconButton(
+                                      icon: Icon(
+                                        Icons.arrow_upward,
+                                        color: Colors.blueAccent,
+                                      ),
+                                      onPressed: () {
+                                        tappedUncheck(index);
+                                      },
+                                    ),
+                                  ])),
+                        )));
+              },
+            ),
+          ),
+        ]),
         floatingActionButton: FloatingActionButton(
           onPressed: () {
             showDialog(
